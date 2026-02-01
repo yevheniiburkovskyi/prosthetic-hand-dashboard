eba9e7c2bb53eddde447440595994625beea07b7
Yevhenii Burkovskyi <yevheniiburkovskyi@gmail.com>
2026-02-01 19:08:13 +0200

    chore: test husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <Button>Click me </Button>
     </div>
   );
 }
