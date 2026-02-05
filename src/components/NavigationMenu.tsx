4ae9cf0390377885a6327023ea4d0e3abf75424c
Yevhenii Burkovskyi <yevhenii.burkovskyi@stormotion.io>
2026-02-05 23:39:08 +0200

    fix: replace div with Link for navigation header in NavigationMenu

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -8,14 +8,15 @@ import {
 } from 'lucide-react';
 import { memo } from 'react';
 import NavigationItem from './ui/NavigationItem';
+import { Link } from 'react-router-dom';
 
 const NavigationMenu = () => {
   return (
     <aside className="min-w-[250px] border-r border-neutral-200 bg-neutral-50 py-4">
-      <div className="mb-4 flex items-center gap-2 px-4">
+      <Link className="mb-4 flex items-center gap-2 px-4" to="/">
         <Activity />
         <h1 className="text-xl">Sensor System</h1>
-      </div>
+      </Link>
       <div className="h-[1px] w-full bg-neutral-200" />
       <nav className="flex flex-col gap-1 px-2 py-4">
         <NavigationItem label={'Overview'} to={'/'} icon={LayoutDashboard} />
